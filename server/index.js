c2bafcc2fdf1a405697f078d255cb78f3bbd198d
Alson Shareef <alson.shareef@gmail.com>
2020-04-02 18:49:28 +0800

    Base server setup including database config to connect to local postgres database.

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const bodyParser = require('body-parser');
+const colors = require('colors');
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Weather App.');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
+);
+
+/**
+ * DATABASE CONFIG
+ */
+const database = require('./database');
+
+database.query('SELECT NOW()', (err, res) => {
+  if (err.error) {
+    return console.log(err.error);
+  }
+  console.log(`PostgreSQL connected: ${res[0].now}.`.blue.bold);
+});
